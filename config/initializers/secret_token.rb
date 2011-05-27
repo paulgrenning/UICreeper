8eeaf2772c663cd90c20282645c0d72bfc40b90c
Jakub Misterka <san.y4ku@gmail.com>
2011-05-27 02:08:21 -0500

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+UICreeper::Application.config.secret_token = '4e6a098ce4685341f325392d9fb0f5c47254ade8a91de3bfc6f9da8828125754da52a891adba30c36b5986c6947d60267ba6704624aa50106ffb3a9ea4af8fb8'
